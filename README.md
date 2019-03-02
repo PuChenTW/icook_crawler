b9c26ff7098f82a66225bdc45baafa039071ef61
ArsonChen <arson@changing.ai>
2019-03-03 00:13:41 +0800

    Add url. Scrap category paths instead of category.

--- README.md
+++ README.md
@@ -3,19 +3,20 @@ This is a Scrapy project to scrape recipes from [icook](https://icook.tw/).
 
 ## Extracted data
 
-This project extracts recipes, combined with the respective title, author, images-url, category, description, ingredients and steps.
+This project extracts recipes, combined with the respective url, title, author, images-url, category paths, description, ingredients and steps.
 The extracted data looks like this sample:
 
-
     {
-        "Tilte": ["台式蘿蔔糕"], 
-        "Author": "[Ca Ca Cooking]", 
-        "images-url": ["https://tokyo-kitchen.icook.network/uploads/recipe/cover/280175/large_c31ca00e545c0521.jpg"], 
-        "Category": ["中式點心"], 
-        "Description": "在家也可以做出簡單美味的台式蘿蔔糕哦~", 
-        "Ingredients": ["在來米粉", "小蝦米", "白蘿蔔", "乾香菇", "油蔥酥", "沙拉油", "鹽", "胡椒粉"], 
-        "Steps": "將白蘿蔔削皮後刨絲，乾香菇泡軟後切丁，小蝦米洗淨瀝乾備用。熱鍋加入1匙的油，將香菇加入香..."
+        "url": "https://icook.tw/recipes/102998", 
+        "Tilte": ["木瓜牛奶凍 [超簡易15分鐘食譜]"], 
+        "Author": "Mrs P's Kitchen", 
+        "images-url": ["https://tokyo-kitchen.icook.network/uploads/recipe/cover/102998/large_59087c73f032e695.jpg"], 
+        "Category paths": [["烘焙點心", "果凍、茶凍"]], 
+        "Description": "這個外形非常可愛的甜點，美味又營養豐富！經常飲木瓜鮮奶，可試試這個甜點，木瓜含豐富的維生素，再加上牛奶，成養顏美肌的甜點。做法超簡單，15分鐘做好，新手不敗，快試試！請也支持我的Facebook 專頁唷*^O^* \n", 
+        "Ingredients": ["糖", "吉利丁粉", "新鮮牛奶", "木瓜"], 
+        "Steps": "先將木瓜去皮，切開一半，用匙羹刮去木瓜籽，去囊。如想多放點奶涷，可刮去一上木瓜肉\n用小鍋煮熱牛奶，下糖，煮至糖溶化，再加入吉利丁粉，不停攪拌至完全溶化即可\n將牛奶倒入木瓜內，待涼後放入冰箱冷藏一小時以上至奶凍完全凝固\n切件享用！"
     }
+
 
 ## Running the spiders
 Install scrapy if you haven't:
--- icook/spiders/recipes_spider.py
+++ icook/spiders/recipes_spider.py
@@ -1,6 +1,29 @@
 import scrapy
 
 
+def create_category_paths(categories: list) -> list:
+    """Construct flat category list into 2D array, remove 全部分類.
+    
+    Arguments:
+        categories {list} -- [description]
+    
+    Returns:
+        list -- [description]
+    """
+    paths = []
+    path = []
+    for category in categories:
+        if category == '全部分類':
+            if path:
+                paths.append(path)
+            path = []
+        else:
+            path.append(category)
+    if path:
+        paths.append(path)
+    return paths
+
+
 class RecipesSpider(scrapy.Spider):
     name = "icook"
     start_urls = [
@@ -17,14 +40,10 @@ class RecipesSpider(scrapy.Spider):
             '//div[@class="browse-recipe-content"]'
             '/a[@class="browse-recipe-name"]/@href')
         ).extract()
-        category_name = response.xpath(
-            '//h2[@class="category-name"]/text()').re(r'(\w+)')
         for url in urls:
-            yield response.follow(url, callback=self.parse_receipes, meta={'category': category_name})
+            yield response.follow(url, callback=self.parse_receipes)
 
-        next_page = response.xpath((
-            '//a[@rel="next"]/@href')
-        ).extract_first()
+        next_page = response.xpath(('//a[@rel="next"]/@href')).extract_first()
 
         if next_page is not None:
             yield response.follow(next_page, callback=self.parse_category)
@@ -34,19 +53,22 @@ class RecipesSpider(scrapy.Spider):
         author = response.xpath(
             '//div[@class="author-name"]/a[@class="author-name-link"]/text()').get()
         images = response.css('img.main-pic::attr(src)').extract()
-        category = response.meta.get('category')
+        categories = response.xpath(
+            '//div[@class="findmore inner-block"]/div[@class="category-tags"]/ul/li/a/text()').getall()
+        category_paths = create_category_paths(categories)
         description = ''.join(response.xpath(
             '//div[@class="header-row description"]/p/text()').getall())
         ingredients = list(
             set(response.css('div.ingredient-name::text').getall()))
-        steps = ''.join(response.xpath(
+        steps = '\n'.join(response.xpath(
             '//li[@class="step"]/div/div/text()').getall())
 
         yield {
+            'url': response.url,
             'Tilte': title,
             'Author': author,
             'images-url': images,
-            'Category': category,
+            'Category paths': category_paths,
             'Description': description,
             'Ingredients': ingredients,
             'Steps': steps,
